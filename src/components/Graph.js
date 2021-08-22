76ce6d37893a6b79bea49c62c51750dc9433dcd3
ateliereven <orly.ev@gmail.com>
2021-08-22 16:56:01 +0300

    Resolves async issue with conditional rendering, sorts fetched data

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,47 +1,57 @@
-import React, { useEffect, useState } from "react";
-import { Line } from 'react-chartjs-2';
-import axios from "axios";
+import React, { useEffect, useState } from "react"
+import { Line } from 'react-chartjs-2'
+import axios from "axios"
 
 function Graph() {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const days = ['09', '10', '11', '12', '13', '14', '15'];
-    const [ratesByDate, setRatesbyDate] = useState([]);
-    console.log(ratesByDate) //returns empty because of async issues inside useEffect
+    const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+    const days = ['09', '10', '11', '12', '13', '14', '15']
+    const [ratesByDate, setRatesbyDate] = useState([])
 
     // get exchange rates from api for 7 dates (days array) and return data for each day (dataByDay array):
     useEffect(async () => {
         const dataByDay = days.map((day) => {
-            const url = `http://api.exchangeratesapi.io/v1/2021-08-${day}?access_key=aeb7c3b01d6a122798a9bead9d4b8ace&symbols=XAU,XAG`;
+            const url = `http://api.exchangeratesapi.io/v1/2021-08-${day}?access_key=d451377251cea73c754f6f86c84d33b2&symbols=XAU,XAG`;
             console.log({ url: url })
             axios.get(url)
                 .then(response => {
                     console.log(response?.data)
-                    setIsLoaded(true)
-                    return response?.data  //doesnt return the response  
+                    setIsLoaded(true) 
+                    setRatesbyDate(ratesByDate => [...ratesByDate, response?.data])
+                    //return response?.data  //doesnt return the response 
                 },
                     error => {
-                        setIsLoaded(true);
+                        setIsLoaded(true)
                         setError(error);
                     }
                 )
         })
-        const results = await Promise.all(dataByDay);
-        setRatesbyDate(ratesByDate => [...ratesByDate, results])
-        console.log(results) //returns undefined. async issue unsolved
+        //const results = await Promise.all(dataByDay);
+        //setRatesbyDate(ratesByDate => [...ratesByDate, results])
+        //console.log(results) //returns undefined. async issue unsolved
     }, [])
 
+    console.log(ratesByDate)
+    const sortedRatesByDate = ratesByDate.sort((a, b) => new Date(a.date) - new Date(b.date))
+    console.log(sortedRatesByDate);
+    //to get the gold-silver ratio from RatesbyDate:
     const ratio = (index) => {
         return (
-            ratesByDate[index].rates.XAG / ratesByDate[index].rates.XAU)
+            sortedRatesByDate[index].rates.XAG / sortedRatesByDate[index].rates.XAU)
     }
-    //returned values would go into data to create the chart:
+
+    if (error) {
+        return <div>Error: {error.message}</div>
+    } else if (!isLoaded || sortedRatesByDate.length < days.length) {
+        return <div>Loading...</div>
+    } else {
+    //returned values would go into data to create a chart:
     const data = {
-        labels: ['ratesByDate[0].date', 'ratesByDate[1].date', 'ratesByDate[2].date', 'ratesByDate[3].date', 'ratesByDate[4].date', 'ratesByDate[5].date', 'ratesByDate[6].date'],//to be replaced with dates from ratesByDate
+        labels: [sortedRatesByDate[0].date, sortedRatesByDate[1].date, sortedRatesByDate[2].date, sortedRatesByDate[3].date, sortedRatesByDate[4].date, sortedRatesByDate[5].date, sortedRatesByDate[6].date],//to be replaced with dates from ratesByDate
         datasets: [
             {
                 label: 'Gold-Silver Ratio',
-                data: ['ratio(0)', 'ratio(1)', 'ratio(2)', 'ratio(3)', 'ratio(4)', 'ratio(5)', 'ratio(6)'], //to be replaced with ratios from ratesByDate
+                data: [ratio(0), ratio(1), ratio(2), ratio(3), ratio(4), ratio(5), ratio(6)], //to be replaced with ratios from ratesByDate
                 lineTension: 0.3,
                 borderColor: ['rgba(255, 206, 86, 0.8)'],
                 backgroundColor: ['rgba(255, 206, 86, 0.4)'],
@@ -76,12 +86,6 @@ function Graph() {
             intersect: false
         }
     }
-
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Loading...</div>;
-    } else {
         return (
             <div>
                 <h3>Gold-Silver Chart</h3>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -24,7 +24,7 @@ function TopBar(props) {
     console.log(latestRates);
     useEffect(() => {
         const url = 'http://api.exchangeratesapi.io/v1/latest' +
-            '?access_key=' + 'aeb7c3b01d6a122798a9bead9d4b8ace' +
+            '?access_key=' + 'd451377251cea73c754f6f86c84d33b2' +
             '&symbols=XAU,XAG,USD,ILS,GBP,BTC';
         console.log({ url: url });
         axios.get(url)
